6c446db6d248320db0796f8c40949cb48005cca8
Gab7barbosa <gabriel.silva1521@aluno.ce.gov.br>
2026-06-22 05:15:49 -0300

    Atualizando o ClienteController.php

--- manga store/app/Http/Controllers/ClienteController.php
+++ manga store/app/Http/Controllers/ClienteController.php
@@ -7,9 +7,7 @@ use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index(Request $request)
     {
         $query = Cliente::query();
@@ -29,17 +27,13 @@ class ClienteController extends Controller
         return view('clientes.index', compact('clientes'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
     {
         return view('clientes.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -55,17 +49,13 @@ class ClienteController extends Controller
         return redirect()->route('clientes.index')->with('success', 'Cliente cadastrado com sucesso!');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
     public function edit(Cliente $cliente)
     {
         return view('clientes.edit', compact('cliente'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    
     public function update(Request $request, Cliente $cliente)
     {
         $validated = $request->validate([
@@ -81,9 +71,7 @@ class ClienteController extends Controller
         return redirect()->route('clientes.index')->with('success', 'Cliente atualizado com sucesso!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(Cliente $cliente)
     {
         $cliente->delete();
